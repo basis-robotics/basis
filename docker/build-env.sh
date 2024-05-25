cd4a804afb91ae2785ddd3e891e0e9d0736938fa
Kyle Franz <zig@Kyles-MacBook-Pro.local>
2024-05-25 16:31:12 -0700

    test

--- docker/build-env.sh
+++ docker/build-env.sh
@@ -1,5 +1,5 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 BASIS_ROOT=$SCRIPT_DIR/..
 
-docker build --tag basis --target basis -f $BASIS_ROOT/docker/Dockerfile $@ $BASIS_ROOT 
-docker build --tag basis-ros --target basis-ros -f $BASIS_ROOT/docker/Dockerfile $@ $BASIS_ROOT 
+docker build --tag basis-env --target basis -f $BASIS_ROOT/docker/Dockerfile $@ $BASIS_ROOT 
+docker build --tag basis-env-ros --target basis-ros -f $BASIS_ROOT/docker/Dockerfile $@ $BASIS_ROOT 
