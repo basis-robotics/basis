d32c7b335b8b41bbf14a52a6eeccb96121302941
Kyle Franz <zig@Kyles-MacBook-Pro.local>
2024-05-25 16:23:50 -0700

    work on docker in ci

--- docker/build-env.sh
+++ docker/build-env.sh
@@ -1,4 +1,5 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 BASIS_ROOT=$SCRIPT_DIR/..
 
-docker build --tag basis-env -f $BASIS_ROOT/docker/Dockerfile $BASIS_ROOT
+docker build --tag basis --target basis -f $BASIS_ROOT/docker/Dockerfile $@ $BASIS_ROOT 
+docker build --tag basis-ros --target basis-ros -f $BASIS_ROOT/docker/Dockerfile $@ $BASIS_ROOT 
