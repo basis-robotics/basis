0cfa0c1890519bd207039ab5418b573b02da43c1
Kyle Franz <zig@Kyles-MacBook-Pro.local>
2024-05-29 11:22:26 -0700

    some small cleanups

--- docker/run-env.sh
+++ docker/run-env.sh
@@ -2,4 +2,4 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 BASIS_ROOT=$SCRIPT_DIR/..
 
 # Note: this relies on macos specific user mapping magic to mount with the proper permissions
-docker run -v $BASIS_ROOT:/basis -it basis-env /bin/bash $@
+docker run -v $BASIS_ROOT:/basis --name basis -it basis-env /bin/bash $@
