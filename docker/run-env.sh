3219ebe4a82f908b22fc9b0e0515b6adb8adffb6
kyle-basis <kyle@basisrobotics.tech>
2024-05-29 11:29:05 -0700

    some small cleanups (#10)

--- docker/run-env.sh
+++ docker/run-env.sh
@@ -1,5 +1,9 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 BASIS_ROOT=$SCRIPT_DIR/..
 
-# Note: this relies on macos specific user mapping magic to mount with the proper permissions
-docker run -v $BASIS_ROOT:/basis -it basis-env /bin/bash $@
+if [ "$(docker ps -a -q -f name=basis)" ]; then
+    docker exec -it basis /bin/bash $@
+else
+    # Note: this relies on macos specific user mapping magic to mount with the proper permissions
+    docker run -v $BASIS_ROOT:/basis --name basis --rm -it basis-env /bin/bash $@
+fi
