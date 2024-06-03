0df3322d59d2c0b140db6a8e56cb8698813698ad
kyle-basis <kyle@basisrobotics.tech>
2024-06-03 12:20:13 -0700

    Add time::now (#19)

--- cpp/core/time/src/time.cpp
+++ cpp/core/time/src/time.cpp
@@ -1,1 +1,21 @@
 #include <basis/core/time.h>
+
+#include <chrono>
+#include <thread>
+#include <time.h>
+
+namespace basis::core {
+MonotonicTime MonotonicTime::FromSeconds(double seconds) { return {TimeBase::SecondsToNanoseconds(seconds)}; }
+
+MonotonicTime MonotonicTime::Now() {
+  timespec ts;
+  clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+
+  return {TimeBase::SecondsToNanoseconds(ts.tv_sec) + ts.tv_nsec};
+}
+
+void MonotonicTime::SleepUntil() const {
+  std::this_thread::sleep_until(std::chrono::steady_clock::time_point(std::chrono::nanoseconds(nsecs)));
+}
+
+} // namespace basis::core
