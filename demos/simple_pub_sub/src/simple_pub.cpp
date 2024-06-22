37e70528221a13fedde50421f764ea8cc4325f56
Thomas Busser <thomas_busser@hotmail.com>
2024-06-22 15:48:50 -0700

    Got it to build and run

---
+++ demos/simple_pub_sub/src/simple_pub.cpp
@@ -0,0 +1,14 @@
+/*
+
+  This is the starting point for your Unit. Edit this directly and implement the missing methods!
+
+*/
+
+#include <simple_pub.h>
+
+using namespace unit::simple_pub;
+
+
+SimplePub::Output simple_pub::SimplePub(const SimplePub::Input& input) {
+    static_assert(false, "Impelement me");
+}
--- demos/simple_pub_sub/src/simple_pub_sub.cpp
+++ demos/simple_pub_sub/src/simple_pub_sub.cpp
@@ -1,4 +1,28 @@
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#include <simple_pub_sub.pb.h>
+#pragma clang diagnostic pop
+
+#include <basis/core/transport/transport_manager.h>
+#include <basis/plugins/transport/tcp.h>
+
+#include "../generated/unit/simple_pub/handlers/SimplePub.h"
+
+using namespace unit::simple_pub::SimplePub;
+
 int main() {
-    return 0;
+  PubSub pubsub(nullptr);
+
+  basis::core::transport::TransportManager transport_manager(
+      std::make_unique<basis::core::transport::InprocTransport>());
+  transport_manager.RegisterTransport(
+      "net_tcp", std::make_unique<basis::plugins::transport::TcpTransport>());
+
+  pubsub.SetupPubSub(&transport_manager, nullptr, nullptr);
+
+  auto msg = std::make_shared<StringMessage>();
+  msg->set_message("Hello, world!");
+  pubsub.out_message_publisher->Publish(msg);
+  return 0;
 }
