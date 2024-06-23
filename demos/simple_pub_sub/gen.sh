7333c33650cacca3e3a618424ed43ed59f5e3ed1
Thomas <thomas_busser@hotmail.com>
2024-06-23 13:10:38 -0700

    create output folders

--- demos/simple_pub_sub/gen.sh
+++ demos/simple_pub_sub/gen.sh
@@ -1,4 +1,5 @@
 #
 # python3 /basis/python/unit/generate_unit.py simple_pub.unit.yaml  ./generated .
-
+mkdir template
+mkdir include
 python3 /basis/python/unit/generate_unit.py simple_pub.unit.yaml  ./generated .
