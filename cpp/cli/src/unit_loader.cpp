04c45ea58714ef4ab3d5a4d4e25295c1aeb9d992
mrzu <thomas_busser@hotmail.com>
2024-07-06 16:40:57 -0700

    Superfluous dlerror()

--- cpp/cli/src/unit_loader.cpp
+++ cpp/cli/src/unit_loader.cpp
@@ -25,7 +25,6 @@ std::unique_ptr<basis::Unit> CreateUnit([[maybe_unused]] const std::filesystem::
   if(maybe_unit_loader == unit_loaders.end()) {
     // For now - need to use RTLD_GLOBAL to allow different inproc transports to communicate
     // This is the opposite of how the protobuf needs things - but no crashes on shutdown (yet?)
-    dlerror();
     void *handle = dlopen(path.c_str(), RTLD_NOW | RTLD_GLOBAL);
     if (!handle) {
       std::cerr << "Failed to dlopen " << path << std::endl;
