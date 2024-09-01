d4b085d8542235f4ff7e1e8475409c77bb9026f8
Kyle Franz <kyle@basisrobotics.tech>
2024-09-01 21:31:10 +0000

    fixed tests

--- cpp/synchronizers/test/test_synchronizers.cpp
+++ cpp/synchronizers/test/test_synchronizers.cpp
@@ -9,38 +9,42 @@
 #pragma clang diagnostic pop
 
 TEST(TestSyncAll, BasicTest) {
-  std::atomic<bool> was_called{false};
-  std::function test_cb = [&](const basis::core::MonotonicTime&, std::shared_ptr<char> a, std::shared_ptr<char> b) {
-    was_called = true;
-    ASSERT_EQ(*a, 'a');
-    ASSERT_EQ(*b, 'b');
-  };
 
-  basis::synchronizers::All<std::shared_ptr<char>, std::shared_ptr<char>> test_all(test_cb);
+  basis::synchronizers::All<std::shared_ptr<char>, std::shared_ptr<char>> test_all;
+
+  std::atomic<bool> was_called{false};
+  std::function test_cb = [&](const std::optional<decltype(test_all)::MessageSumType> &maybe_tuple) {
+    if (maybe_tuple) {
+      was_called = true;
+      auto &[a, b] = *maybe_tuple;
+      ASSERT_EQ(*a, 'a');
+      ASSERT_EQ(*b, 'b');
+    }
+  };
 
   auto a = std::make_shared<char>('a');
   auto b = std::make_shared<char>('b');
 
   // Shouldn't be called
   test_all.OnMessage<0>(a);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_FALSE(was_called);
   // Shouldn't be called
   test_all.OnMessage<0>(a);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_FALSE(was_called);
 
   test_all.OnMessage<1>(b);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_TRUE(was_called);
   was_called = false;
 
   test_all.OnMessage<1>(b);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_FALSE(was_called);
 
   test_all.OnMessage<0>(a);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_TRUE(was_called);
 }
 
@@ -49,13 +53,16 @@ TEST(TestSyncAll, TestOptional) {
   std::shared_ptr<char> recvd_a;
   std::shared_ptr<char> recvd_b;
 
-  basis::synchronizers::All<std::shared_ptr<char>, std::shared_ptr<char>> test_all(
-      [&](const basis::core::MonotonicTime&, auto a, auto b) {
-        was_called = true;
-        recvd_a = a;
-        recvd_b = b;
-      },
-      {}, {.is_optional = true});
+  basis::synchronizers::All<std::shared_ptr<char>, std::shared_ptr<char>> test_all({}, {.is_optional = true});
+
+  std::function test_cb = [&](const std::optional<decltype(test_all)::MessageSumType> &maybe_tuple) {
+    if (maybe_tuple) {
+      was_called = true;
+      auto [a, b] = *maybe_tuple;
+      recvd_a = a;
+      recvd_b = b;
+    }
+  };
 
   auto a = std::make_shared<char>('a');
   auto b = std::make_shared<char>('b');
@@ -64,56 +71,58 @@ TEST(TestSyncAll, TestOptional) {
   ASSERT_EQ(recvd_b, nullptr);
 
   test_all.OnMessage<0>(a);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_EQ(*recvd_a, 'a');
   ASSERT_EQ(recvd_b, nullptr);
   recvd_a = nullptr;
 
   test_all.OnMessage<1>(b);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_EQ(recvd_a, nullptr);
   ASSERT_EQ(recvd_b, nullptr);
 
   test_all.OnMessage<0>(a);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_EQ(*recvd_a, 'a');
   ASSERT_EQ(*recvd_b, 'b');
 }
 
 TEST(TestSyncAll, TestCached) {
   std::atomic<bool> was_called{false};
-  std::function test_cb = [&](const basis::core::MonotonicTime&, std::shared_ptr<char> a, std::shared_ptr<char> b) {
-    was_called = true;
-    ASSERT_EQ(*a, 'a');
-    ASSERT_EQ(*b, 'b');
+
+  basis::synchronizers::All<std::shared_ptr<char>, std::shared_ptr<char>> test_all({}, {.is_cached = true});
+  std::function test_cb = [&](const std::optional<decltype(test_all)::MessageSumType> &maybe_tuple) {
+    if (maybe_tuple) {
+      was_called = true;
+      auto &[a, b] = *maybe_tuple;
+      ASSERT_EQ(*a, 'a');
+      ASSERT_EQ(*b, 'b');
+    }
   };
-
-  basis::synchronizers::All<std::shared_ptr<char>, std::shared_ptr<char>> test_all(test_cb, {}, {.is_cached = true});
-
   auto a = std::make_shared<char>('a');
   auto b = std::make_shared<char>('b');
 
   ASSERT_EQ(was_called, false);
 
   test_all.OnMessage<0>(a);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_EQ(was_called, false);
 
   test_all.OnMessage<1>(b);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_EQ(was_called, true);
   was_called = false;
 
   test_all.OnMessage<0>(a);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_EQ(was_called, true);
   was_called = false;
 
   test_all.OnMessage<1>(b);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_EQ(was_called, false);
   test_all.OnMessage<0>(a);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_EQ(was_called, true);
 }
 
@@ -123,11 +132,15 @@ TEST(TestSyncAll, TestContainer) {
   std::shared_ptr<char> recvd_a;
   std::vector<std::shared_ptr<char>> recvd_b;
 
-  basis::synchronizers::All<std::shared_ptr<char>, std::vector<std::shared_ptr<char>>> test_all([&](const basis::core::MonotonicTime&, auto a, auto b) {
-    was_called = true;
-    recvd_a = a;
-    recvd_b = b;
-  });
+  basis::synchronizers::All<std::shared_ptr<char>, std::vector<std::shared_ptr<char>>> test_all;
+  std::function test_cb = [&](const std::optional<decltype(test_all)::MessageSumType> &maybe_tuple) {
+    if (maybe_tuple) {
+      auto &[a, b] = *maybe_tuple;
+      was_called = true;
+      recvd_a = a;
+      recvd_b = b;
+    }
+  };
 
   auto a = std::make_shared<char>('a');
   auto b1 = std::make_shared<char>('1');
@@ -138,12 +151,12 @@ TEST(TestSyncAll, TestContainer) {
   ASSERT_EQ(recvd_b.size(), 0);
 
   test_all.OnMessage<0>(a);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_EQ(recvd_a, nullptr);
   ASSERT_EQ(recvd_b.size(), 0);
 
   test_all.OnMessage<1>(b1);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_EQ(recvd_a, a);
   ASSERT_EQ(recvd_b.size(), 1);
   ASSERT_EQ(recvd_b, decltype(recvd_b)({b1}));
@@ -152,20 +165,20 @@ TEST(TestSyncAll, TestContainer) {
   recvd_b.clear();
 
   test_all.OnMessage<1>(b1);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_EQ(recvd_a, nullptr);
   ASSERT_EQ(recvd_b.size(), 0);
   test_all.OnMessage<1>(b2);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_EQ(recvd_a, nullptr);
   ASSERT_EQ(recvd_b.size(), 0);
   test_all.OnMessage<1>(b3);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_EQ(recvd_a, nullptr);
   ASSERT_EQ(recvd_b.size(), 0);
 
   test_all.OnMessage<0>(a);
-  test_all.ConsumeIfReady(basis::core::MonotonicTime::Now());
+  test_cb(test_all.ConsumeIfReady());
   ASSERT_EQ(recvd_a, a);
 
   ASSERT_EQ(recvd_b, decltype(recvd_b)({b1, b2, b3}));
@@ -193,61 +206,58 @@ TEST(TestSyncField, BasicTest) {
       basis::synchronizers::Field<std::shared_ptr<const OuterSyncTestStruct>,
                                   [](const OuterSyncTestStruct *outer) { return outer->header().stamp(); }>,
       basis::synchronizers::Field<std::vector<std::shared_ptr<const Unsynced>>, nullptr>>
-      test([](const basis::core::MonotonicTime&, std::shared_ptr<const Foo>, std::shared_ptr<const OuterSyncTestStruct>,
-              std::vector<std::shared_ptr<const Unsynced>>) {
-
-      });
+      test;
 
   auto unsynced = std::make_shared<Unsynced>(0xFF);
 
   // Check that we sync at all
   // [1], [], []
   test.OnMessage<0>(std::make_shared<Foo>(2));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   // [1], [2], []
   test.OnMessage<1>(produce_proto(1));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   // [1], [2], [X]
   test.OnMessage<2>(unsynced);
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   // [1, 2], [2], [X] (sync on 2)
   test.OnMessage<1>(produce_proto(2));
-  ASSERT_TRUE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_TRUE(test.ConsumeIfReady());
   // [], [], []
 
   // Check that when we sync, we leave data in the buffer for later
   // [], [], [X]
   test.OnMessage<2>(unsynced);
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   // [3], [], [X]
   test.OnMessage<1>(produce_proto(3));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   // [3, 4], [], [X]
   test.OnMessage<1>(produce_proto(4));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   // [3, 4], [3], [X] (sync on 3)
   test.OnMessage<0>(std::make_shared<Foo>(3));
-  ASSERT_TRUE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_TRUE(test.ConsumeIfReady());
   // [4], [], []
 
   // [4], [], [X]
   test.OnMessage<2>(unsynced);
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   // [4], [4], [X] (sync on 4)
   test.OnMessage<0>(std::make_shared<Foo>(4));
-  ASSERT_TRUE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_TRUE(test.ConsumeIfReady());
   // [], [], []
 
   // Test that when we sync, we still wait for unsynced messages
 
   // [5], [5], [] (sync on 5, but no output)
   test.OnMessage<0>(std::make_shared<Foo>(5));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   test.OnMessage<1>(produce_proto(5));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   // [5], [5], [X]
   test.OnMessage<2>(unsynced);
-  ASSERT_TRUE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_TRUE(test.ConsumeIfReady());
 }
 
 struct ApproxTest {
@@ -259,39 +269,39 @@ TEST(TestSyncField, TestApproximate) {
       0.01, basis::synchronizers::Field<std::shared_ptr<const ApproxTest>, &ApproxTest::f>,
       basis::synchronizers::Field<std::shared_ptr<const ApproxTest>, &ApproxTest::f>,
       basis::synchronizers::Field<std::vector<std::shared_ptr<const Unsynced>>, nullptr>>
-      test({}, {}, {}, {.is_cached = true});
+      test({}, {}, {.is_cached = true});
 
   // Test the same number, but no third memeber
   test.OnMessage<0>(std::make_shared<ApproxTest>(0.0));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   test.OnMessage<1>(std::make_shared<ApproxTest>(0.0));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   auto unsynced = std::make_shared<Unsynced>(0xFF);
   test.OnMessage<2>(unsynced);
-  ASSERT_TRUE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_TRUE(test.ConsumeIfReady());
   // Shouldn't need unsynced after this as we are cached
 
   // Test the same number, again
   test.OnMessage<0>(std::make_shared<ApproxTest>(1.0));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   test.OnMessage<1>(std::make_shared<ApproxTest>(1.0));
-  ASSERT_TRUE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_TRUE(test.ConsumeIfReady());
 
   // Test numbers that are too far apart
   test.OnMessage<0>(std::make_shared<ApproxTest>(2.0));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   test.OnMessage<1>(std::make_shared<ApproxTest>(3.0));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
 
   // Test numbers that are similar
   test.OnMessage<0>(std::make_shared<ApproxTest>(3.001));
-  ASSERT_TRUE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_TRUE(test.ConsumeIfReady());
 
   // Test numbers that aren't quite similar enough
   test.OnMessage<0>(std::make_shared<ApproxTest>(4.0));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   test.OnMessage<1>(std::make_shared<ApproxTest>(4.011));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
 }
 
 struct TypeConversionString {
@@ -310,17 +320,17 @@ TEST(TestSyncField, TestTypeConversion) {
       test;
 
   test.OnMessage<0>(std::make_shared<TypeConversionString>("foobar"));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   test.OnMessage<1>(std::make_shared<TypeConversionInt>(1));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   test.OnMessage<0>(std::make_shared<TypeConversionString>("1"));
-  ASSERT_TRUE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_TRUE(test.ConsumeIfReady());
   test.OnMessage<0>(std::make_shared<TypeConversionString>("33"));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   test.OnMessage<0>(std::make_shared<TypeConversionString>("42"));
-  ASSERT_FALSE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_FALSE(test.ConsumeIfReady());
   test.OnMessage<1>(std::make_shared<TypeConversionInt>(33));
-  ASSERT_TRUE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_TRUE(test.ConsumeIfReady());
   test.OnMessage<1>(std::make_shared<TypeConversionInt>(42));
-  ASSERT_TRUE(test.ConsumeIfReady(basis::core::MonotonicTime::Now()));
+  ASSERT_TRUE(test.ConsumeIfReady());
 }
