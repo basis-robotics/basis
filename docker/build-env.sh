c7f3a63c028548e562cabc1a70d15efd67bab3d7
Kyle Franz <kyle@basisrobotics.tech>
2024-09-05 14:44:55 -0700

    tweak docker build

--- docker/build-env.sh
+++ docker/build-env.sh
@@ -2,5 +2,10 @@ set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 BASIS_ROOT=$SCRIPT_DIR/..
 
+BASIS_ENABLE_ROS=${BASIS_ENABLE_ROS:1}
+
 docker build --tag basis-env --target basis-env -f $BASIS_ROOT/docker/Dockerfile $@ $BASIS_ROOT 
-docker build --tag basis-env-ros --target basis-env-ros -f $BASIS_ROOT/docker/Dockerfile $@ $BASIS_ROOT 
+
+if ${BASIS_ENABLE_ROS}; then
+    docker build --tag basis-env-ros --target basis-env-ros -f $BASIS_ROOT/docker/Dockerfile $@ $BASIS_ROOT 
+fi
