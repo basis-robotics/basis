2b23aeb79aef6f504f415166d379f6376e18440c
Kyle Franz <kyle@basisrobotics.tech>
2024-10-27 10:58:45 -0700

    comments

---
+++ cpp/plugins/bridges/foxglove/src/foxglove_dummy_main.cpp
@@ -0,0 +1,2 @@
+// This file should be left empty - it's needed to give CMake a source file, 
+// which is needed if you have a binary target that is solely the result of linking other targets together
