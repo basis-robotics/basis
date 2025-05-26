cfe46a856d5a0d307f4837ba9f0be9c431e2eb84
Kyle Franz <kyle@basisrobotics.tech>
2025-05-26 19:04:06 +0000

    cleanup

--- unit/pybind_test/include/pybind_test.h
+++ unit/pybind_test/include/pybind_test.h
@@ -5,32 +5,32 @@
 
 */
 
-#include <unit/pybind_test/unit_base.h>
 #include <Python.h>
+#include <unit/pybind_test/unit_base.h>
 
-#define X_PYTHON_API \
-    X_PY(Py_InitializeEx) \
-    X_PY(Py_Finalize) \
-    X_PY(Py_IsInitialized) \
-    X_PY(PyRun_SimpleStringFlags) \
-    X_PY(PyEval_SaveThread) \
-    X_PY(PyEval_RestoreThread) \
-    X_PY(PyGILState_Ensure) \
-    X_PY(PyGILState_Release)
-    
+#define X_PYTHON_API                                                           \
+  X_PY(Py_InitializeEx)                                                        \
+  X_PY(Py_Finalize)                                                            \
+  X_PY(Py_IsInitialized)                                                       \
+  X_PY(PyRun_SimpleStringFlags)                                                \
+  X_PY(PyEval_SaveThread)                                                      \
+  X_PY(PyEval_RestoreThread)                                                   \
+  X_PY(PyGILState_Ensure)                                                      \
+  X_PY(PyGILState_Release)
+
 class pybind_test : public unit::pybind_test::Base {
 public:
   pybind_test(const Args &args,
-         const std::optional<std::string_view> &name_override = {});
+              const std::optional<std::string_view> &name_override = {});
   virtual unit::pybind_test::InprocTest::Output
   InprocTest(const unit::pybind_test::InprocTest::Input &input) override;
-  virtual unit::pybind_test::InprocTestTrigger::Output
-  InprocTestTrigger(const unit::pybind_test::InprocTestTrigger::Input &input) override;
- 
+  virtual unit::pybind_test::InprocTestTrigger::Output InprocTestTrigger(
+      const unit::pybind_test::InprocTestTrigger::Input &input) override;
+
 private:
-  #define X_PY(f) decltype(::f)* f = nullptr;
+#define X_PY(f) decltype(::f) *f = nullptr;
   X_PYTHON_API
-  #undef X_PY
+#undef X_PY
 
   bool pub = false;
 
--- unit/pybind_test/include/python_shim.h
+++ unit/pybind_test/include/python_shim.h
@@ -7,22 +7,23 @@
 extern "C" {
 #endif
 
-// TODO: if we used C++ we could use 
+// TODO: if we used C++ we could use
 typedef struct {
-    void* (*pthread_getspecific)(pthread_key_t);
-    int (*pthread_setspecific)(pthread_key_t, const void*);
-    int (*pthread_key_create)(pthread_key_t*, void (*)(void*));
-    int (*pthread_key_delete)(pthread_key_t);
-    int (*pthread_create) (pthread_t *__restrict __newthread,
-			   const pthread_attr_t *__restrict __attr,
-			   void *(*__start_routine) (void *),
-			   void *__restrict __arg) __THROWNL __nonnull ((1, 3));
-    char* (*setlocale)(int category, const char *locale);
+  void *(*pthread_getspecific)(pthread_key_t);
+  int (*pthread_setspecific)(pthread_key_t, const void *);
+  int (*pthread_key_create)(pthread_key_t *, void (*)(void *));
+  int (*pthread_key_delete)(pthread_key_t);
+  int (*pthread_create)(pthread_t *__restrict __newthread,
+                        const pthread_attr_t *__restrict __attr,
+                        void *(*__start_routine)(void *),
+                        void *__restrict __arg) __THROWNL __nonnull((1, 3));
+  char *(*setlocale)(int category, const char *locale);
 } pthread_shim_table_t;
 
-extern pthread_shim_table_t* shim_pthread_table;
+extern pthread_shim_table_t *shim_pthread_table;
 
 #ifdef __cplusplus
 }
 #endif
+
 #endif
