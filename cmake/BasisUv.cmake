32604ccbe6e5d4bb41ace4185a83c1ba79d34a1c
Kyle Franz <kyle@basisrobotics.tech>
2025-07-07 00:20:45 +0000

    Swap to fetchcontent

--- cmake/BasisUv.cmake
+++ cmake/BasisUv.cmake
@@ -1,9 +1,16 @@
-include(uvtarget/Uv)
-
 function(basis_uv_initialize PYPROJECT_FILE)
+    include(FetchContent)
+    FetchContent_Declare(
+        uvtarget
+        GIT_REPOSITORY https://github.com/basis-robotics/uvtarget
+        GIT_TAG main
+    )
+    FetchContent_MakeAvailable(uvtarget)
+
+    include(${uvtarget_SOURCE_DIR}/Uv.cmake)
     uv_initialize(
         PYTHON_VERSION ${BASIS_PYTHON_VERSION}
-        UNMANAGED_PYPROJECT_FILE ${PYPROJECT_FILE}
+        MANAGED_PYPROJECT_FILE ${PYPROJECT_FILE}
         WORKSPACE_PACKAGE_NAME basis_cmake
         INSTALLATION_VENV /opt/basis/.venv
         INSTALLATION_VENV_CACHE /opt/basis/cache
